b7eac1de9c328b66ffbed884c0609c612fa9d5b3
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-08-23 12:47:37 +0530

    Add files via upload

---
+++ spring-boot-config/src/main/java/io/anush/springbootconfig/GreetingController.java
@@ -0,0 +1,50 @@
+package io.anush.springbootconfig;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	@Value("${my.greeting: default value}")
+	private String greetingMessage;
+
+	@Value("some static message")
+	private String staticMessage;
+
+	@Value("${my.list.values}")
+	private List<String> listValues;
+
+	// @Value("#{${dbValues}}")
+	// private Map<String,String> dbValues;
+
+	@Autowired
+	private DbSettings dbSettings;
+
+	@Autowired
+	private Environment env;
+
+	public GreetingController() {
+
+	}
+
+	@GetMapping("/greeting")
+	private String greeting() {
+		// return greetingMessage + staticMessage + listValues ;
+		return dbSettings.getConnection() + dbSettings.getHost();
+
+	}
+
+	@GetMapping("/envdetails")
+	public String envDetails() {
+
+		return env.toString();
+	}
+
+}
